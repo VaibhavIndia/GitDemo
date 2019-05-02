ce72c6f266485661be09f28fcf9d6bd077425225
vaibhav <vaibhav.nigade@gmail.com>
2019-05-02 12:44:12 +0530

    first commit

---
+++ src/one/Team.java
@@ -0,0 +1,17 @@
+package one;
+
+public class Team {
+    Member member;
+    public Team(Member member) { 
+        this.member = member; 
+    }
+    // here's our main method
+    public static void main(String[] args) {
+       Member myMember = new Member("Aurieel", "light", 10, 1);
+       Team myTeam = new Team(myMember);
+       System.out.println(myTeam.member.getName());
+       System.out.println(myTeam.member.getType());
+       System.out.println(myTeam.member.getLevel());
+       System.out.println(myTeam.member.getRank());
+    }
+}
